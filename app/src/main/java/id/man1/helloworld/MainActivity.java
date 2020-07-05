3f12fef1a6060883781ad89e34e27ed52ede218e
Adam Mubarok <adammubarok@gmail.com>
2020-07-05 14:42:59 +0700

    use plain putExtra

--- app/src/main/java/id/man1/helloworld/MainActivity.java
+++ app/src/main/java/id/man1/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartNewActivity(View view) {
+
+        EditText name = (EditText) findViewById(R.id.editTextName);
+        EditText surName = (EditText) findViewById(R.id.editTextSurName);
+        EditText email = (EditText) findViewById(R.id.editTextEmail);
+
         Intent myIntent = new Intent(this, SecondActivity.class);
+        myIntent.putExtra("name", name.getText().toString());
+        myIntent.putExtra("surname", surName.getText().toString());
+        myIntent.putExtra("email", email.getText().toString());
+
         startActivity(myIntent);
     }
 }
--- app/src/main/java/id/man1/helloworld/SecondActivity.java
+++ app/src/main/java/id/man1/helloworld/SecondActivity.java
@@ -2,7 +2,9 @@ package id.man1.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
+
+        Intent i = getIntent();
+        String name = i.getStringExtra("name");
+        String surName = i.getStringExtra("surname");
+        String email = i.getStringExtra("email");
+
+        TextView tvName = (TextView) findViewById(R.id.textViewName);
+        TextView tvSurName = (TextView) findViewById(R.id.textViewSurName);
+        TextView tvEmail = (TextView) findViewById(R.id.textViewEmail);
+
+        tvName.setText(name);
+        tvSurName.setText(surName);
+        tvEmail.setText(email);
+;    }
 }
